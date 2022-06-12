91203fe323b734871c72d56345a7e1fc7b8016cc
tjasz <nukorausa@hotmail.com>
2022-06-12 14:54:40 -0700

    Allow 10 guesses

--- src/util.ts
+++ src/util.ts
@@ -7,7 +7,7 @@ export enum Difficulty {
 }
 
 export const gameName = "hello wordl";
-export const maxGuesses = 6;
+export const maxGuesses = 10;
 
 export const dictionarySet: Set<string> = new Set(dictionary);
 
